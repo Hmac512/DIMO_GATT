48df9d63789f375de23540ebedd445fc9388aa56
Hersh Patel <hersh@dimo.zone>
2022-03-24 12:37:28 -0400

    Adding error

--- gatt/gatt.py
+++ gatt/gatt.py
@@ -301,53 +301,55 @@ def main():
         reply_handler=register_app_cb,
         error_handler=register_app_error_cb,
     )
+    try:
+        device_manager = dbus.Interface(bus.get_object("org.bluez", "/"),
+                                        "org.freedesktop.DBus.ObjectManager")
+        all_objects = device_manager.GetManagedObjects()
+        all_devices = (str(path) for path, interfaces in all_objects.iteritems() if
+                       "org.bluez.Device1" in interfaces.keys())
 
-    device_manager = dbus.Interface(bus.get_object("org.bluez", "/"),
-                                    "org.freedesktop.DBus.ObjectManager")
-    all_objects = device_manager.GetManagedObjects()
-    all_devices = (str(path) for path, interfaces in all_objects.iteritems() if
-                   "org.bluez.Device1" in interfaces.keys())
+        for path, interfaces in all_objects.iteritems():
+            if "org.bluez.Adapter1" not in interfaces.keys():
+                continue
 
-    for path, interfaces in all_objects.iteritems():
-        if "org.bluez.Adapter1" not in interfaces.keys():
-            continue
+            print("[ " + path + " ]")
 
-        print("[ " + path + " ]")
-
-        properties = interfaces["org.bluez.Adapter1"]
-        for key in properties.keys():
-            value = properties[key]
-            if (key == "UUIDs"):
-                list = extract_uuids(value)
-                print("    %s = %s" % (key, list))
-            else:
-                print("    %s = %s" % (key, value))
-
-        device_list = [d for d in all_devices if d.startswith(path + "/")]
-
-        for dev_path in device_list:
-            print("    [ " + dev_path + " ]")
-
-            dev = all_objects[dev_path]
-            properties = dev["org.bluez.Device1"]
-
+            properties = interfaces["org.bluez.Adapter1"]
             for key in properties.keys():
                 value = properties[key]
                 if (key == "UUIDs"):
                     list = extract_uuids(value)
-                    print("        %s = %s" % (key, list))
-                elif (key == "Class"):
-                    print("        %s = 0x%06x" % (key, value))
-                elif (key == "Vendor"):
-                    print("        %s = 0x%04x" % (key, value))
-                elif (key == "Product"):
-                    print("        %s = 0x%04x" % (key, value))
-                elif (key == "Version"):
-                    print("        %s = 0x%04x" % (key, value))
+                    print("    %s = %s" % (key, list))
                 else:
-                    print("        %s = %s" % (key, value))
+                    print("    %s = %s" % (key, value))
 
-        print("")
+            device_list = [d for d in all_devices if d.startswith(path + "/")]
+
+            for dev_path in device_list:
+                print("    [ " + dev_path + " ]")
+
+                dev = all_objects[dev_path]
+                properties = dev["org.bluez.Device1"]
+
+                for key in properties.keys():
+                    value = properties[key]
+                    if (key == "UUIDs"):
+                        list = extract_uuids(value)
+                        print("        %s = %s" % (key, list))
+                    elif (key == "Class"):
+                        print("        %s = 0x%06x" % (key, value))
+                    elif (key == "Vendor"):
+                        print("        %s = 0x%04x" % (key, value))
+                    elif (key == "Product"):
+                        print("        %s = 0x%04x" % (key, value))
+                    elif (key == "Version"):
+                        print("        %s = 0x%04x" % (key, value))
+                    else:
+                        print("        %s = %s" % (key, value))
+
+            print("")
+    except Exception as e:
+        traceback.print_exc()
 
     mainloop.run()
 
